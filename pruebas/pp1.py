b89d9891ada6290e434ab9268849d9b8576359f6
VCAngel <vargascasavantesangel.329@gmail.com>
2022-06-29 10:49:44 -0600

    Reestructure QGraphics Components

--- pruebas/pp1.py
+++ pruebas/pp1.py
@@ -1,54 +1,106 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QPushButton , QGraphicsView, QGraphicsItem
-from PyQt5.QtGui import QBrush, QPen, QFont
-from PyQt5.QtCore import Qt
+from tkinter import Y
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsItem
+from PyQt5.QtGui import QBrush, QPen
+from PyQt5.QtCore import Qt, QRectF, QPointF, QLineF
 import sys
-from PyQt5.QtCore import QPointF, QLineF, QRectF, QRegExp
- 
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
- 
-        self.setWindowTitle("Pyside2 QGraphic View")
-        self.setGeometry(300,200,640,520)
- 
-        self.scene = QGraphicsScene(self)
 
-        self.view = QGraphicsView(self.scene, self)
-        self.view.setGeometry(0,0,640, 440)
- 
-        
+
+class Canvas(QGraphicsItem):
+    # * Canvas Component. Controls Drawing
+    def __init__(self, helper):
+        super(Canvas, self).__init__()
+
+        # Referencia a la escena padre. Permite acceder a las funciones de dibujo
+        self.parentScene = helper.scene
+
+        # Brushes y Pens
         self.greenBrush = QBrush(Qt.green)
-        self.blueBrush = QBrush(Qt.blue)
- 
         self.blackPen = QPen(Qt.black)
         self.blackPen.setWidth(5)
-        self.first_draw = True
-        self.show()
-        
+
+        self.firstDraw = True
+
+        # Variables de seguimiento
+        self.first_point = None
+        self.prev_point = None
+
+    def paint(self, painter, option, widget):
+        painter.setOpacity(1)
+        painter.fillRect(self.boundingRect(), Qt.white)
+
+    def boundingRect(self):
+        drawArea = QRectF(0,0,400,400)
+        return drawArea
 
     def mousePressEvent(self, e):
         x = e.pos().x()
         y = e.pos().y()
+
+        print(x,y)
+
         if e.button() == 2:
             # If a polygon is being drawn, finish the polygon by clicking right mouse button. This will close the
             # polygon and remove the lines drawn as support to show the polygon and replace them with the actual
             # edges and points of the polygon
-            if self.first_draw:
+            if self.firstDraw:
                 pass
             else:
-                line = self.scene.addLine(QLineF(self.prev_point, self.first_point))
+                self.parentScene.addLine(
+                    QLineF(self.prev_point, self.first_point), self.blackPen)
         elif e.button() == 1:
-            if self.first_draw:
-                point = self.scene.addEllipse(x - 3, y - 3, 6, 6)
+            if self.firstDraw:
+                self.parentScene.addEllipse(
+                    x - 3, y - 3, 6, 6, self.blackPen, self.greenBrush)
                 self.prev_point = QPointF(x, y)
                 self.first_point = QPointF(x, y)
-                self.first_draw = False
+                self.firstDraw = False
             else:
-                point = self.scene.addEllipse(x - 3, y - 3, 6, 6)
-                line = self.scene.addLine(QLineF(self.prev_point, QPointF(x, y)))
+                self.parentScene.addEllipse(
+                    x - 3, y - 3, 6, 6, self.blackPen, self.greenBrush)
+                self.parentScene.addLine(
+                    QLineF(self.prev_point, QPointF(x, y)), self.blackPen)
                 self.prev_point = QPointF(x, y)
-                
- 
-app = QApplication(sys.argv)
-window = Window()
-sys.exit(app.exec_())
+
+
+class MainView(QGraphicsView):
+    # * Window's Main View. The main camera per se
+    def __init__(self):
+        super(MainView, self).__init__()
+
+        # Crear escena para los items dentro del View
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        self.setMouseTracking = QGraphicsScene(self)
+
+        # Agregar el componente Canvas a la escena
+        self.canvas = Canvas(self)
+        self.scene.addItem(self.canvas)
+
+
+class Window(QMainWindow):
+    # * Main Application Window
+    def __init__(self, screenSize=None):
+        super(Window, self).__init__()
+        self.setWindowTitle("Pyside2 QGraphic View - Draw Test")
+
+        self.view = MainView()
+        self.setCentralWidget(self.view)
+
+        # Centrar en pantalla
+        if screenSize is not None:
+            center = (screenSize.width()/2, screenSize.height()/2)
+            self.setGeometry(int(center[0]), int(center[1]), 640, 480)
+        else:
+            self.setGeometry(0, 0, 640, 480)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    screen = app.primaryScreen()
+
+    # Crear ventana
+    window = Window(screen.size())
+    # Mostrar ventana
+    window.show()
+
+    sys.exit(app.exec_())
