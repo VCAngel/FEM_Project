663b9916211d568c32db9e9c3e44b8bb5bf13b0a
VCAngel <vargascasavantesangel.329@gmail.com>
2022-06-29 12:08:45 -0600

    Add polygon tracking on mouseEvent

--- pruebas/canvas.py
+++ pruebas/canvas.py
@@ -1,6 +1,6 @@
 from tkinter import Y
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsItem
-from PyQt5.QtGui import QBrush, QPen
+from PyQt5.QtGui import QBrush, QPen, QPolygonF
 from PyQt5.QtCore import Qt, QRectF, QPointF, QLineF
 import sys
 
@@ -18,48 +18,70 @@ class Canvas(QGraphicsItem):
         self.blackPen = QPen(Qt.black)
         self.blackPen.setWidth(5)
 
-        self.firstDraw = True
+        self.newPoly = True
 
         # Variables de seguimiento
         self.first_point = None
         self.prev_point = None
+        self.currentPoly = None
+
+        # Listas de seguimiento
+        self.polyList = []
+        self.edgeList = []
+        self.holeList = []
 
     def paint(self, painter, option, widget):
         painter.setOpacity(1)
         painter.fillRect(self.boundingRect(), Qt.white)
 
     def boundingRect(self):
-        drawArea = QRectF(0,0,400,400)
+        drawArea = QRectF(0, 0, 400, 400)
         return drawArea
 
     def mousePressEvent(self, e):
         x = e.pos().x()
         y = e.pos().y()
 
-        print(x,y)
-
         if e.button() == 2:
             # If a polygon is being drawn, finish the polygon by clicking right mouse button. This will close the
             # polygon and remove the lines drawn as support to show the polygon and replace them with the actual
             # edges and points of the polygon
-            if self.firstDraw:
+            if self.newPoly or self.currentPoly.__len__() <= 2:
                 pass
             else:
                 self.parentScene.addLine(
                     QLineF(self.prev_point, self.first_point), self.blackPen)
+
+                # Agregar poligono a lista
+                self.polyList.append(self.currentPoly)
+
+                # Resetear estado inicial
+                self.currentPoly = None
+                self.newPoly = True
+                self.first_point = None
+                self.prev_point = None
         elif e.button() == 1:
-            if self.firstDraw:
+            if self.newPoly:
+                # Inicializar nuevo poligono
+                self.currentPoly = QPolygonF()
+
                 self.parentScene.addEllipse(
                     x - 3, y - 3, 6, 6, self.blackPen, self.greenBrush)
-                self.prev_point = QPointF(x, y)
                 self.first_point = QPointF(x, y)
-                self.firstDraw = False
+                self.prev_point = QPointF(x, y)
+
+                # Pasar el punto inicial al Poligono a construir
+                self.currentPoly << self.first_point
+                self.newPoly = False
             else:
                 self.parentScene.addEllipse(
                     x - 3, y - 3, 6, 6, self.blackPen, self.greenBrush)
                 self.parentScene.addLine(
                     QLineF(self.prev_point, QPointF(x, y)), self.blackPen)
                 self.prev_point = QPointF(x, y)
+
+                # Pasar el punto previo al Poligono a construir
+                self.currentPoly << self.prev_point
 
 
 class MainView(QGraphicsView):
